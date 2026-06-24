4efc0bcd622fc40791e89b90c54cfd09d93d9baf
thrisnz <thris@email.com>
2026-06-25 09:55:31 +1000

    Update app.py

--- app.py
+++ app.py
@@ -50,14 +50,14 @@ BASE_URL = "https://api-fxtrade.oanda.com/v3"
 ADD_THRESHOLD = 0.03
 MIN_UNITS = 1
 
-SL_ENABLED = False
+SL_ENABLED = True
 
 # ===== LADDER (PERCENT BASED) =====
 BE_LEVEL   = 0.00125
 BE_BUFFER  = 0.0005   # 🔥 important: avoids instant stopout at BE
 LOCK1_TRIG = 0.0033
 LOCK1_LOCK = 0.0018
-LOCK2_TRIG = 0.0048
+LOCK2_TRIG = 0.0045
 LOCK2_LOCK = 0.0033
 
 # ===== STATE =====
