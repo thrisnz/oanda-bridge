f1a986474addc9b4dd46e1488be1b31bae03b90a
thrisnz <thris@email.com>
2026-07-03 22:02:24 +1000

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ SL_ENABLED = True
 # ===== LADDER (PERCENT BASED) =====
 BE_LEVEL   = 0.00125
 BE_BUFFER  = 0.001   # 🔥 important: avoids instant stopout at BE
-LOCK1_TRIG = 0.0033
+LOCK1_TRIG = 0.0036
 LOCK1_LOCK = 0.0018
 LOCK2_TRIG = 0.0045
 LOCK2_LOCK = 0.0033
