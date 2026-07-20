68b1e5b569d325139a0d496a5d3e86df66764e65
thrisnz <thris@email.com>
2026-07-20 10:34:02 +1000

    Update app.py

--- app.py
+++ app.py
@@ -54,11 +54,11 @@ SL_ENABLED = True
 
 # ===== LADDER (PERCENT BASED) =====
 BE_LEVEL   = 0.00125
-BE_BUFFER  = 0.001   # 🔥 important: avoids instant stopout at BE
-LOCK1_TRIG = 0.0043
-LOCK1_LOCK = 0.0010
-LOCK2_TRIG = 0.0060
-LOCK2_LOCK = 0.0033
+BE_BUFFER  = 0.0010   # 🔥 important: avoids instant stopout at BE
+LOCK1_TRIG = 0.0051
+LOCK1_LOCK = 0.0020
+LOCK2_TRIG = 0.0078
+LOCK2_LOCK = 0.0040
 
 # ===== STATE =====
 sl_trades = {}
